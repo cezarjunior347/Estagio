0035782ec9383be45da2ab7bdc875a465aab46fc
Cezar Barroso Pinto Junior <cezarjr347@gmail.com>
2024-09-05 03:48:28 -0300

    arquivo

--- questao_3.py
+++ questao_3.py
@@ -14,31 +14,31 @@ import pandas as pd
 
 def calcular_faturamento(dados):
     # Filtrar dias com faturamento maior que 0
-    dias_com_faturamento = dados[dados['faturamento'] > 0]
+    dias_com_faturamento = dados[dados['valor'] > 0]
 
     # Calculando o menor e o maior valor de faturamento
-    menor_faturamento = min(dias_com_faturamento['faturamento'])
-    maior_faturamento = max(dias_com_faturamento['faturamento'])
+    menor_faturamento = min(dias_com_faturamento['valor'])
+    maior_faturamento = max(dias_com_faturamento['valor'])
 
     # Calculando a média mensal
-    media_mensal = dias_com_faturamento['faturamento'].mean()
+    media_mensal = dias_com_faturamento['valor'].mean()
 
    
     # Contando os dias com faturamento acima da média
-    #dias_acima_da_media = dias_com_faturamento[dias_com_faturamento['faturamento'] > media_mensal]['faturamento'].count()
-    dias_acima_media = (dias_com_faturamento['faturamento'] > media_mensal).sum()
+    #dias_acima_media = dias_com_faturamento[dias_com_faturamento['valor'] > media_mensal]['valor'].count()
+    dias_acima_media = (dias_com_faturamento['valor'] > media_mensal).sum()
 
     return menor_faturamento, maior_faturamento, dias_acima_media
 
 # Carregar dados do arquivo JSON
-dados = pd.read_json('teste.json')
+dados = pd.read_json('dados.json')
 
 # Calcular os valores solicitados
 menor_faturamento, maior_faturamento, dias_acima_media = calcular_faturamento(dados)
 
 # Exibir resultados
-print(f"Menor valor de faturamento: {menor_faturamento}")
-print(f"Maior valor de faturamento: {maior_faturamento}")
+print(f"Menor valor de faturamento: {menor_faturamento:.2f}")
+print(f"Maior valor de faturamento: {maior_faturamento:.2f}")
 print(f"Número de dias com faturamento superior à média: {dias_acima_media}")
 
 
